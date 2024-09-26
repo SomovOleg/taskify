64313d43613af60fa1a54903608b3f13e92e8b6b
cruasanya <novs2104@gmail.com>
2024-09-26 16:15:02 +0300

    added docker compose file

--- firebaseconfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAr7hdb9IqQJhoVvzbTyOfLXim96EUM5MY",
-    authDomain: "app-taskify.firebaseapp.com",
-    projectId: "app-taskify",
-    storageBucket: "app-taskify.appspot.com",
-    messagingSenderId: "752843564170",
-    appId: "1:752843564170:web:109a27ed1569e264d83973"
-};
--- interfaces/user.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface UserInterface {
-    uid: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-}
